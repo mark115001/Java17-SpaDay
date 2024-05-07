4e3d7985a6f9315cd2012851e445410f1e17a6d5
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-05-06 19:53:25 -0500

    Chapter 14 - Class 11 Studio

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,17 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/user/add")
+    public void displayAddUserForm() {
+        return add;
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,44 @@
+package org.launchcode.models;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
